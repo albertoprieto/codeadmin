be578fce0e95163adb021afc7b65ad2c9239f807
dev jap <alberto.preto.r@gmail.com>
2024-11-10 22:53:08 -0600

    Deploy to GitHub Pages

--- src/main.js
+++ src/main.js
@@ -1,35 +1,32 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
 //**primevue */
 import PrimeVue from 'primevue/config';
 import 'primeicons/primeicons.css';
-import Aura from '@primevue/themes/aura';
+import '@primevue/resources/themes/saga-blue/theme.css'; // Asegúrate de que el tema esté importado correctamente
 import ToastService from 'primevue/toastservice';
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
 // Importar componentes de PrimeVue
 import Button from 'primevue/button';
 import Menubar from 'primevue/menubar';
 import Toast from 'primevue/toast';
 
-const app = createApp(App)
-app.use(PrimeVue, {
-    // Default theme configuration
-    theme: {
-        preset: Aura,
-        options: {
-            prefix: 'p',
-            darkModeSelector: 'system',
-            cssLayer: false
-        }
-    }
- });
-app.use(ToastService)
+// Crear la aplicación Vue
+const app = createApp(App);
+
+// Usar PrimeVue y otros servicios
+app.use(PrimeVue);
+app.use(ToastService);
+app.use(router);
+app.use(createPinia());
+
+// Registrar componentes globalmente
 app.component('Button', Button);
 app.component('Menubar', Menubar);
 app.component('Toast', Toast);
-app.use(createPinia())
-app.use(router)
-app.mount('#app')
+
+// Montar la aplicación
+app.mount('#app');
