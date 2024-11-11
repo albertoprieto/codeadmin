451e6f64996e1645d095ec48cc7d3399b3ab93c4
dev jap <alberto.preto.r@gmail.com>
2024-11-10 22:34:18 -0600

    Deploy to GitHub Pages

--- src/main.js
+++ src/main.js
@@ -2,17 +2,17 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 //**primevue */
-import PrimeVue from '/primevue/config';
-import '/primeicons/primeicons.css';
+import PrimeVue from 'primevue/config';
+import 'primeicons/primeicons.css';
 import Aura from '@primevue/themes/aura';
-import ToastService from '/primevue/toastservice';
+import ToastService from 'primevue/toastservice';
 import App from './App.vue'
 import router from './router'
 
-// Importar componentes de PrimeVue 
-import Button from '/primevue/button';
-import Menubar from '/primevue/menubar';
-import Toast from '/primevue/toast';
+// Importar componentes de PrimeVue
+import Button from 'primevue/button';
+import Menubar from 'primevue/menubar';
+import Toast from 'primevue/toast';
 
 const app = createApp(App)
 app.use(PrimeVue, {
