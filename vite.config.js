e534acb1c28bf520e723717ffdfc657f88b43695
dev jap <alberto.preto.r@gmail.com>
2024-11-10 20:30:41 -0600

    Actualizar configuracion 2

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/codeadmin/', // Reemplaza 'codeadmin' con el nombre de tu repositorio
+  base: '/codeadmin/',
   plugins: [
     vue(),
   ],
